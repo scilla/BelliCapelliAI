9171916d82a58e6c8c014b64edf4a28dffaf3fea
AntonioP <thebomb.mg@gmail.com>
2025-06-01 18:46:57 +0200

    refactor: remove side buttons from PhoneMock component

--- client/src/components/ui/phone-mock.tsx
+++ client/src/components/ui/phone-mock.tsx
@@ -18,12 +18,6 @@ export default function PhoneMock({ children }: PhoneMockProps) {
           {/* Home indicator */}
           <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2 w-32 h-1 bg-white/40 rounded-full"></div>
         </div>
-        
-        {/* Side buttons */}
-        <div className="absolute left-0 top-20 w-1 h-8 bg-gray-700 rounded-r"></div>
-        <div className="absolute left-0 top-32 w-1 h-12 bg-gray-700 rounded-r"></div>
-        <div className="absolute left-0 top-48 w-1 h-12 bg-gray-700 rounded-r"></div>
-        <div className="absolute right-0 top-24 w-1 h-16 bg-gray-700 rounded-l"></div>
       </div>
       
       {/* Full screen for mobile */}
